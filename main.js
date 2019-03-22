0411b68c103428064a0575519e8e6f9ee32787ba
ziyueyanggg <46909548+ziyueyanggg@users.noreply.github.com>
2019-03-22 23:32:07 +0800

    update

---
+++ main.js
@@ -0,0 +1,9 @@
+// app.js
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
+Vue.config.productionTip = false;
